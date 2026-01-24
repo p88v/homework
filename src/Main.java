13851da3276b2576ecfc2e1fd6db76c6565310db
p88 <ereschko.m@yandex.ru>
2026-01-24 12:40:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Выберите операцию: ");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("0. Выход из программы");
+            System.out.println("1. Добавить дело");
+            System.out.println("2. Показать дела");
+            System.out.println("3. Удалить дело по номеру");
+            System.out.println("4. Удалить дело по названию");
+            System.out.println("5. Удаление по ключевому слову");
+            System.out.print("Ваш выбор: ");
+
+            String input = scanner.nextLine();
+
+            if (input.equals("0")) {
+                break;
+            } else if (input.equals("1")) {
+                System.out.print("Введите дело которое хотите добавить: ");
+                String cas = scanner.nextLine();
+                list.add(cas);
+            } else if (input.equals("2")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("Дело номер " + (i + 1) + ": " + list.get(i));
+                }
+            } else if (input.equals("3")) {
+                System.out.print("Выберите номер элемента для удаления: ");
+                String del = scanner.nextLine();
+                int rem = Integer.parseInt(del);
+                for (int i = 0; i < list.size(); i++) {
+                    rem -= 1;
+                    if (rem == i) {
+                        list.remove(rem);
+                        System.out.println("Удаление завершено успешно.");
+                    } else System.out.println("Ошибка удаления попробуйте еще раз.");
+                }
+            } else if (input.equals("4")) {
+                System.out.print("Введите текст заметки для удаления: ");
+                String remForWords = scanner.nextLine();
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.contains(remForWords)) {
+                        list.remove(i);
+                        System.out.println("Удаление по слову прошло успешно.");
+                    } else System.out.println("Ошибка при удалении, попробуйте еще раз.");
+                }
+            } else if (input.equals("5")) {
+                System.out.print("Введите ключевое слово для удаления: ");
+                String inp = scanner.nextLine();
+
+                for (int i = list.size() - 1; i >= 0; i--) {
+                    if (list.contains(inp)) {
+                        list.remove(i);
+                        System.out.println("Удаление прошло успешно.");
+                    } else System.out.println("Сопадений не найдено.");
+                }
+
+            }
+
+
+        }
+    }
+}
