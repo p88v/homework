2065170b2ab0f81fe5f4b6f615b93ae8b6bfa7c7
p88v <ereschko.m@yandex.ru>
2026-02-18 21:06:48 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         List<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+
         while (true) {
 
             System.out.println("Выберите операцию: ");
@@ -29,26 +30,30 @@ public class Main {
                 System.out.print("Введите дело которое хотите добавить: ");
                 String cas = scanner.nextLine();
                 list.add(cas);
+                showNotes(list);
             } else if (input.equals("2")) {
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println("Дело номер " + (i + 1) + ": " + list.get(i));
-                }
+                showNotes(list);
             } else if (input.equals("3")) {
-                System.out.print("Выберите номер элемента для удаления: ");
-                String del = scanner.nextLine();
-                int rem = Integer.parseInt(del);
-                rem -= 1;
-                if(!list.contains(rem)){
+
+                try{
+                    System.out.print("Выберите номер элемента для удаления: ");
+                    String del = scanner.nextLine();
+                    int rem = Integer.parseInt(del) - 1;
+                    list.remove(rem);
+                    showNotes(list);
+                } catch (IndexOutOfBoundsException e){
                     System.out.println("Такого номера дела в списке нет");
                 }
-                list.remove(rem);
+
+
             } else if (input.equals("4")) {
                 System.out.print("Введите текст заметки для удаления: ");
                 String remForWords = scanner.nextLine();
-                    if ( list.remove(remForWords)) {
-                        System.out.println("Удаление по слову прошло успешно.");
-                    } else System.out.println("Ошибка при удалении, попробуйте еще раз.");
+                if ( list.remove(remForWords)) {
+                    System.out.println("Удаление по слову прошло успешно.");
 
+                } else System.out.println("Ошибка при удалении, попробуйте еще раз.");
+                showNotes(list);
             } else if (input.equals("5")) {
                 System.out.print("Введите ключевое слово для удаления: ");
                 String inp = scanner.nextLine();
@@ -59,10 +64,13 @@ public class Main {
                         System.out.println("Удаление прошло успешно.");
                     } else System.out.println("Сопадений не найдено.");
                 }
-
+                showNotes(list);
             }
-
-
+        }
+    }
+    public static void showNotes(List<String> list){
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Дело номер " + (i + 1) + ": " + list.get(i));
         }
     }
 }
